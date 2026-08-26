e52eeab9520b179da7e276272b0b7acc9ec6a267
Mangobject-oriented <josephdezuman@gmail.com>
2026-08-26 12:32:38 +0800

    Add APK, notification test, workflow and Android files

---
+++ functions/test-notification.js
@@ -0,0 +1,43 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require(
+  "../checker/serviceAccountKey.json"
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+async function sendTestNotification() {
+  try {
+    await admin.messaging().send({
+      topic: "detect_co_announcements",
+
+      notification: {
+        title: "DETECT-CO ANNOUNCEMENT",
+        body: "gumagana ba o meron ng iba",
+      },
+
+      android: {
+        notification: {
+          channelId: "test_alerts",
+          sound: "test_alert",
+        },
+      },
+
+      data: {
+        type: "test",
+        message: "Manual notification test",
+      },
+    });
+
+    console.log(
+      "Test notification sent successfully!"
+    );
+  } catch (error) {
+    console.error("ERROR:");
+    console.error(error);
+  }
+}
+
+sendTestNotification();
